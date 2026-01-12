f0c1ee316c7fa184d845264696ca84aa3082d60a
dmdgpdi(malone) <33450285+dmdgpdi@users.noreply.github.com>
2026-01-13 04:44:37 +0900

    feat: weather entitiy 모델 구현

---
+++ src/entities/weather/api/WeatherSeriesRepository.implement.ts
@@ -0,0 +1,3 @@
+import type { WeatherSeriesRepository } from "../model/WeatherSeriesRepository.interface";
+
+interface WeatherSeriesRepositoryImplement extends WeatherSeriesRepository {}
---
+++ src/entities/weather/model/TimeRange.type.ts
@@ -0,0 +1,6 @@
+export interface TimeRange {
+  /**UTC */
+  start: Date;
+  /**UTC */
+  end: Date;
+}
