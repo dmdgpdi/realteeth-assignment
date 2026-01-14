de838f669ea67081868a9326be8b810efb29a5b7
dmdgpdi(malone) <33450285+dmdgpdi@users.noreply.github.com>
2026-01-14 23:26:42 +0900

    chore: 빌드할 때 방해되는 요소 제거

--- app/detail/page.tsx
+++ app/detail/page.tsx
@@ -1,3 +1,11 @@
+import { Suspense } from "react";
 import { DetailPage } from "@/pages";
+import { Skeleton } from "@/shared/ui/skeleton";
 
-export default DetailPage;
+export default function Page() {
+  return (
+    <Suspense fallback={<Skeleton className="h-dvh w-dvw" />}>
+      <DetailPage />
+    </Suspense>
+  );
+}
