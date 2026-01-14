b8bbf9dcde8bd772e8185b47744f3b949920b31c
dmdgpdi(malone) <33450285+dmdgpdi@users.noreply.github.com>
2026-01-14 21:04:58 +0900

    feat: renameFavoriteLocation optimistic update 추가

--- src/features/favoriteLocation/index.ts
+++ src/features/favoriteLocation/index.ts
@@ -1,5 +1,4 @@
-import { useRenameFavoriteLocation } from "./renameFavoriteLocation/useRenameFavoriteLocation.model";
 import { ToggleFavoriteLocationIconButton } from "./toggleFavoriteLocation/ui/ToggleFavoriteLocationIconButton";
 
-export { useRenameFavoriteLocation, ToggleFavoriteLocationIconButton };
+export { ToggleFavoriteLocationIconButton };
 export { useToggleFavoriteLocation } from "./toggleFavoriteLocation/useToggleFavoriteLocation.model";
