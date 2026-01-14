df5f06aa60ddb1d33c46ac7c890389eee39ae089
dmdgpdi(malone) <33450285+dmdgpdi@users.noreply.github.com>
2026-01-14 23:14:29 +0900

    chore: mockRepo대신 다른 실제 API 연결

--- src/app/RootProvider.tsx
+++ src/app/RootProvider.tsx
@@ -6,18 +6,16 @@ import { FavoriteLocationRepositoryProvider } from "@/entities/favoriteLocation/
 import { KakaoLocationRepository } from "@/entities/location/api/KakaoLocationRepository/KakaoLocationRepository";
 import { LocationRepositoryProvider } from "@/entities/location/model/LocationRepositoryProvider";
 import { WeatherSeriesRepositoryImplement } from "@/entities/weather/api/WeatherSeriesRepository.implement";
-import { WeatherSeriesRepositoryMock } from "@/entities/weather/api/WeatherSeriesRepository.mock";
 import { DailyWeatherSeriesRepositoryProvider } from "@/entities/weather/model/DailyWeatherSeriesProvider";
 
 const locationRepo = new KakaoLocationRepository();
 const weatherRepo = WeatherSeriesRepositoryImplement;
 const favoriteRepo = FavoriteLocationRepositoryImpl;
-const weatherRepsMock = WeatherSeriesRepositoryMock;
 
 export function RootProvider({ children }: PropsWithChildren) {
   return (
     <LocationRepositoryProvider repo={locationRepo}>
-      <DailyWeatherSeriesRepositoryProvider repo={weatherRepsMock}>
+      <DailyWeatherSeriesRepositoryProvider repo={weatherRepo}>
         <FavoriteLocationRepositoryProvider repo={favoriteRepo}>
           {children}
         </FavoriteLocationRepositoryProvider>
